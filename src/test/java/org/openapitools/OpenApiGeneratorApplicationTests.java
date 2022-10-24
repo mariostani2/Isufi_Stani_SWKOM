d0223a1ee1664e7d49e6df84d6ef17b6a41bd6aa
Reldi <if20b006@technikum-wien.at>
2022-10-24 11:38:46 +0200

    Test change

--- src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
+++ src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
@@ -2,8 +2,9 @@ package org.openapitools;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class OpenApiGeneratorApplicationTests {
 
     @Test
